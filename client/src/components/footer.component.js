4b5b50641697112a6ea023aeaa4f6a088f2f17c0
lukasjoho <social@lukashoppe.com>
2020-03-21 15:51:03 +0100

    edit footer

--- client/src/components/footer.component.js
+++ client/src/components/footer.component.js
@@ -15,8 +15,9 @@ export default class Footer extends Component {
               <div class="col-md-6 mt-md-0 mt-3">
         
                 <h5 class="text-uppercase font-weight-bold">Motivation</h5>
-                <p>Die Idee ist zu Beginn der Corona-Kirse entstanden. Innerhalb unserer Freudnesgruppe wurden viele themenreleveante Artikel und Inhalte gepostet.
-                    Um dort den Überblick zu behalten und meinen Freunden die Möglichkeit zu geben, ihre Artikel selber hinzuzufügen, kam die Idee zu dieser kleinen App.
+                <p>Die Idee ist zu Beginn der Corona-Krise entstanden. Innerhalb unserer Freundesgruppe wurden viele themenrelevante Artikel und Inhalte gepostet.
+                    Um dort den Überblick zu behalten und meinen Freunden die Möglichkeit zu geben, ihre Artikel selber hinzuzufügen, kam die Idee zu dieser Mini-App.
+                    Ich möchte damit hauptsächlich lernen, wie man Apps strukturiert aufbaut und welche technologischen Abläufe dahinter stehen. 
                 </p>
         
               </div>
@@ -27,7 +28,8 @@ export default class Footer extends Component {
         
                 <h5 class="text-uppercase font-weight-bold">technologischer Hintergrund</h5>
                 <p>Die App ist mit dem MERN-Stack gebaut. Das Projekt gab mir die Möglichkeit mich näher mit MongoDB, Express, React und NodeJS vertraut zu machen. 
-                    Ich habe gelernt Backend und Frontend mit einander zu koppeln, einen Server mit Express zu bauen und HTTP-Requests in Verbindung mit meiner Database auf MongoDB durchzuführen. 
+                    Ich habe gelernt Backend und Frontend mit einander zu koppeln, einen Server mit Express zu bauen und mir mit HTTP-Requests Daten vom Server zu holen.
+                    Vllt. werde ich Zukunft noch etwas weiter daran bauen, um mir beispielsweise User Authentication und Bewertungssysteme (Up-/Downvotes) anzuschauen. 
                 </p>
         
               </div>
--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -13,19 +13,21 @@ export default class Navbar extends Component {
       <nav className="navbar navbar-dark navbar-expand-lg">
         <div className="container">
         <Link to="/" className="navbar-brand">Knowrona</Link>
-        <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link"><FontAwesomeIcon class="icon" icon={faThList}/><span class="nav-text">Posts</span></Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link"><FontAwesomeIcon  class="icon" icon={faFile}/><span class="nav-text">Create Post</span></Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/user" className="nav-link"><FontAwesomeIcon class="icon" icon={faUserPlus}/><span class="nav-text">Create User</span> </Link>
-          </li>
-        </ul>
-        </div>
+          <div className="collpase navbar-collapse">
+            <div class="navbar-flex">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+              <Link to="/" className="nav-link"><FontAwesomeIcon class="icon" icon={faThList}/><span class="nav-text">Posts</span></Link>
+              </li>
+              <li className="navbar-item">
+              <Link to="/create" className="nav-link"><FontAwesomeIcon  class="icon" icon={faFile}/><span class="nav-text">Create Post</span></Link>
+              </li>
+              <li className="navbar-item">
+              <Link to="/user" className="nav-link"><FontAwesomeIcon class="icon" icon={faUserPlus}/><span class="nav-text">Create User</span> </Link>
+              </li>
+            </ul>
+            </div>
+          </div>
         </div>
       </nav>
     );
