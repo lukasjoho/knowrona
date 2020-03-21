01a948ace0c325d48224b438a2d936c99de2d769
lukasjoho <social@lukashoppe.com>
2020-03-21 10:40:22 +0100

    Version 1.0

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ const usersRouter = require("./routes/users");
 app.use("/posts", postsRouter);
 app.use("/users", usersRouter);
 
+// if (process.env.NODE_ENV === "production"){
+//     app.use(express.static("client/build"));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "./client", "build", "index.html"));
+//     });
+// }
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
