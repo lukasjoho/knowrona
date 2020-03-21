3f2f407d886f1ba48afe7a57ec7ea2b26ec10da1
lukasjoho <social@lukashoppe.com>
2020-03-21 14:39:16 +0100

    enable get

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ const usersRouter = require("./routes/users");
 app.use("/posts", postsRouter);
 app.use("/users", usersRouter);
 
-// if (process.env.NODE_ENV === "production"){
-//     app.use(express.static("client/build"));
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "./client", "build", "index.html"));
-//     });
-// }
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "./client", "build", "index.html"));
+    });
+}
 
 
 const port = process.env.PORT || 5000;
